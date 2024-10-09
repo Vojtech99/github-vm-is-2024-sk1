1f93b2506e9c383cc83d8950708c27da22c84c18
Vojtěch Macháček <vojtech.machacek@tul.cz>
2024-10-09 10:06:49 +0200

    #1 základní program

---
+++ IS_Projekty/program000 - zakladni kod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+
+        string again = "a";
+
+        while(again=="a") { //dve rovnáse jsou porovnání
+            Console.Clear();
+            Console.WriteLine("**************************"); 
+            Console.WriteLine("**** Výpis řady čísel ****");  
+            Console.WriteLine("**************************"); 
+            Console.WriteLine("**** Vojtěch Macháček ****");   
+            Console.WriteLine("**************************"); 
+            Console.WriteLine();
+
+        
+
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu A");
+            again = Console.ReadLine();       //jedno rovnáse je přikázání
+
+
+        }
+
+
+
+    }
+}
+
+
+//toto je jednořádový komentář
+
+/* toto je
+víceřádkový 
+komentář */
+
+// tab = posun řádku , shift+tab posun na druhou stranu
+
+
+
+
+
+
