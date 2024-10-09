ca949eb4f4155886edce4f0c0cdaee100255b1fc
Vojtěch Macháček <vojtech.machacek@tul.cz>
2024-10-09 09:42:54 +0200

    #1 výpis řady - základ programu s opakováním

--- IS_Projekty/001-prvni-program-vypis-rady/Program.cs
+++ IS_Projekty/001-prvni-program-vypis-rady/Program.cs
@@ -1,5 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ahoj.");
-Console.WriteLine("joujou");
-Console.WriteLine("jezusky");
-Console.ReadKey();
+﻿using System;
+
+class Program {
+    static void Main() {
+
+        string again = "a";
+
+        while(again=="a") { //dve rovnáse jsou porovnání
+            Console.Clear();
+            Console.WriteLine("**************************"); 
+            Console.WriteLine("**** Výpis řady čísel ****");  
+            Console.WriteLine("**************************"); 
+            Console.WriteLine("**** Vojtěch Macháček ****");   
+            Console.WriteLine("**************************"); 
+            Console.WriteLine();
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu A");
+            again = Console.ReadLine();       //jedno rovnáse je přikázání
+
+
+        }
+
+
+
+    }
+}
+
+
+//toto je jednořádový komentář
+
+/* toto je
+víceřádkový 
+komentář */
+
+// tab = posun řádku , shift+tab posun na druhou stranu
+
+
+
+
+
+
