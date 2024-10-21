56b1b26f9eab008ca22cf444d454b14ed1ee8f68
Vojtěch Macháček <vojtech.machacek@tul.cz>
2024-10-21 10:57:09 +0200

    #3 pravoúhlý trojúhelník

---
+++ IS_Projekty/program003b-pravouhly-trojuhelnik/Program.cs
@@ -0,0 +1,38 @@
+﻿string again = "a";
+
+        while(again=="a") { 
+            Console.Clear();
+            Console.WriteLine("*******************************"); 
+            Console.WriteLine("**** Pravoúhlý trojuhelník ****");  
+            Console.WriteLine("*******************************"); 
+            Console.WriteLine("******* Vojtěch Macháček ******");   
+            Console.WriteLine("*******************************"); 
+            Console.WriteLine();
+
+
+
+
+               Console.Write("Zadejte výšku obrazce (celé číslo): ");
+            int height;
+            while(!int.TryParse(Console.ReadLine(), out height)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte výšku znovu: ");
+
+            }
+
+        
+            for (int i = 1; i <= height; i++) {
+                for (int j = 1; j <= i; j++){
+                    System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+                    Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+
+        
+
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu A");
+            again = Console.ReadLine();       
+
+
+        }
