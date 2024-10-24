fe03e295c0e0a8d138a00a09abeb6078002772cc
Vojtěch Macháček <vojtech.machacek@tul.cz>
2024-10-24 15:47:35 +0200

    #4 obrazec písmenko V

---
+++ IS_Projekty/program003c-obrazec-pismenko-v/Program.cs
@@ -0,0 +1,52 @@
+﻿
+        string again = "a";
+
+        while(again=="a") { 
+            Console.Clear();
+            Console.WriteLine("******************************"); 
+            Console.WriteLine("**** Obrazec - Písmenko V ****");  
+            Console.WriteLine("******************************"); 
+            Console.WriteLine("****** Vojtěch Macháček ******");   
+            Console.WriteLine("******************************"); 
+            Console.WriteLine();
+
+            Console.Write("Zadejte výšku obrazce (celé číslo): ");
+            int height;
+            while(!int.TryParse(Console.ReadLine(), out height)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte výšku obrazce znovu: ");
+            }
+
+
+
+             for (int i = 0; i < height; i++) {
+                System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+                for (int j = 0; j < i; j++){
+                Console.Write(" ");
+            }
+             Console.Write("*");
+
+            for (int j = 0; j < (2* (height - i -1)); j++){
+                System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+                
+                Console.Write(" ");
+            }
+
+            if (i != height ){ 
+                System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+                Console.Write("*");
+            }
+
+            Console.WriteLine("");
+        }
+    
+
+
+        
+
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu A");
+            again = Console.ReadLine();       
+
+
+        }
+
