82250952cfc7ae5d9030602fbbc385efb7855de0
Vojtěch Macháček <vojtech.machacek@tul.cz>
2024-10-25 15:12:41 +0200

    #4 obrazec 2

---
+++ IS_Projekty/program003j-obrazec-2/Program.cs
@@ -0,0 +1,47 @@
+﻿ string again = "a";
+
+        while(again=="a") { 
+            Console.Clear();
+            Console.WriteLine("*********************"); 
+            Console.WriteLine("**** Obrazec - 2 ****");  
+            Console.WriteLine("*********************"); 
+            Console.WriteLine("** Vojtěch Macháček *");   
+            Console.WriteLine("*********************"); 
+            Console.WriteLine();
+
+            Console.Write("Zadejte výšku obrazce (celé číslo): ");
+            int height;
+            while(!int.TryParse(Console.ReadLine(), out height)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte výšku obrazce znovu: ");
+            }
+
+            for(int i = 0;i < height; i++){
+                for(int j = 0; j < height ; j++){ 
+                    if(i == 0||i == height -1||j== 0||j ==height-1){
+                        Console.Write("*");
+                    }
+                    else if(j==height/2){
+                        Console.Write("*");
+                    }
+                      else if(i==height/2){
+                        Console.Write("*");
+                    }
+                    else{
+                        Console.Write(" ");
+                    }
+              
+                  
+                }
+                Console.WriteLine("");
+            }
+    
+
+
+        
+
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu A");
+            again = Console.ReadLine();       
+
+
+        }
